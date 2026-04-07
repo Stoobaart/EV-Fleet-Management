94a414e2ba00a0e301f1093d217aafbda849b5c8
Stu <stuartgordonsmith5@gmail.com>
2026-04-07 21:06:28 +0100

    updated drivers table structure to match vehicles page

--- src/features/drivers/pages/DriversPage.jsx
+++ src/features/drivers/pages/DriversPage.jsx
@@ -1,5 +1,10 @@
 import { DriversTable } from '../components/DriversTable'
 
 export default function DriversPage() {
-  return <DriversTable />
+  return (
+    <main>
+      <h1>Drivers</h1>
+      <DriversTable />
+    </main>
+  )
 }
